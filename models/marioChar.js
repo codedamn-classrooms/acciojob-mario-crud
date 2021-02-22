f6f774e7a46a7e5491587a3a44fbecda1e51bfb6
Mehul Mohan <mehulmpt@gmail.com>
2021-02-22 19:01:42 +0530

    default files

--- models/marioChar.js
+++ models/marioChar.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 
 //  Your code goes here
+const marioModel = new mongoose.Schema({
+    // your model code
+}, { collection: 'marios' })
 
 module.exports = marioModel
